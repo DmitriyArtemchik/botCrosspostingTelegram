91909e893ecf0129d32c7dcf8980e42dd50af2c0
DmitriyArtemchik <69394565+DmitriyArtemchik@users.noreply.github.com>
2022-11-08 23:51:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const telegramApi = require('node-telegram-bot-api');
 const token = '5636675071:AAErQXS-JvZhbLOIYYno5JmNqLzKy-FzE_E';
+import TeleBot from "telebot";
 
 const bot = telegramApi(token, {polling: true});
 
+const bot2 = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+
+bot2.on('text', msg => msg.reply.text(msg.text))
+
+export default bot2
 
 bot.on('message', msg => {
     console.log(msg);
